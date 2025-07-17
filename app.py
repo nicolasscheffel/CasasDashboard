11022fa9bb3454afc7d30a86f3a76da195e0480a
nicolasscheffel <nicolasscheffel@hotmail.com>
2025-07-16 21:18:07 -0300

    alteração do app.py Usa a porta que o Render fornece (os.environ.get('PORT')

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ app.layout = html.Div([
 server = app.server  # necessário para o Render
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8050))  # Render define a variável PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
